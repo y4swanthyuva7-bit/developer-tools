766c217ba72d0810458a36f5a50f770a21b2ffd5
y4swanthyuva7-bit <y4swanthyuva7@gmail.com>
2026-09-15 10:36:59 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,8 @@
+length = float(input("Enter length: "))
+breadth = float(input("Enter breadth: "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print("Area =", area)
+print("Perimeter =", perimeter)
